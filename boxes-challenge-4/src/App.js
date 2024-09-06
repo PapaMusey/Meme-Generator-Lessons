adb22c5014dd04a7a316359eac2b69b219caac72
Andy Musey <p.emusey@gmail.com>
2024-09-06 08:52:00 +0000

    box-lesson 4 completed

--- boxes-challenge-4/src/App.js
+++ boxes-challenge-4/src/App.js
@@ -8,7 +8,17 @@ import Boxes from './Boxes';
 function App(props) {
   const [squares,setSquares] = React.useState(Boxes)
   function toggle(id){
-    console.log(id)
+    setSquares(prevSquares => {
+      return prevSquares.map(square => {
+         return square.id === id ? {...square, on: !square.on} : square
+      })
+  })
+  /*Does square id match the id I have selected? If so, 
+  then return a new object that has all the properties of square 
+  except for the on property which you would have to change to the opposite of what it was (Boolean)
+
+  The reason why "{...square, on: !square.on}" is in curly bracket is because it's an object {}
+ */
   }
 
   const newSquareArray = squares.map (square => (
